ee3743d47b0dc5e657b1a627f59fde777c1e6f8c
Mohan1459 <157452661+Mohan1459@users.noreply.github.com>
2026-01-22 12:28:05 +0530

    Create addition_assign.c
    In this operation the LHS operand adds to the RHS operand, and the result is assigned to the LHS.

---
+++ OPERATORS/ASSIGNMENT_OPERATORS/ASSIGNMENT_ADDITION_OPERATOR/addition_assign.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("ENTER THE VALUE OF A=");
+    scanf("%d",&a);
+    printf("ENTER THE VALUE OF B=");
+    scanf("%d",&b);
+    b+=a;
+    printf("The value of b=%0d",b);
+    return 0;
+}
