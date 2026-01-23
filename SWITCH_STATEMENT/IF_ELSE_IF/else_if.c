c4f33365bbb012ae2cd50e19920521a0d019de58
Mohan1459 <157452661+Mohan1459@users.noreply.github.com>
2026-01-23 11:30:49 +0530

    Create else_if.c

---
+++ SWITCH_STATEMENT/IF_ELSE_IF/else_if.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int number;
+    printf("ENTER THE NUMBER =" );
+    scanf("%d",&number);
+    if(number==10)
+    printf("NUMBER EQUAL TO %0d",number);
+     else if(number==20)
+    printf("NUMBER EQUAL TO %0d",number);
+     else if(number==40)
+    printf("NUMBER EQUAL TO %0d",number);
+    else
+     printf("NUMBER NOT EQUAL");
+    return 0;
+}
