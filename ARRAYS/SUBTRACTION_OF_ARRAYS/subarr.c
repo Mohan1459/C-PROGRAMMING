4fafd854077a45b141e0c1de29e37058b468fc73
Mohan1459 <157452661+Mohan1459@users.noreply.github.com>
2026-04-30 12:36:53 +0530

    Create subarr.c

---
+++ ARRAYS/SUBTRACTION_OF_ARRAYS/subarr.c
@@ -0,0 +1,70 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define N 50
+int main()
+{
+    int i,j,m,n,p,q,k;
+    int a[N][N],b[N][N],c[N][N];
+    printf("enter the rows of first matrix:");
+    scanf("%d",&m);
+    printf("Enter the columns of first matrix:");
+    scanf("%d",&n);
+    for(i=0;i<m;i++)
+    {
+       for(j=0;j<n;j++)
+       {
+           printf("Enter the elements of the first matrix:");
+           scanf("%d",&a[i][j]);
+       }
+    }
+    printf("\nThe first matrix is\n");
+    for(i=0;i<m;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+         printf("%d\t",a[i][j]);
+        }
+         printf("\n");
+    }
+
+
+    printf("Enter the rows of second matrix:");
+    scanf("%d",&p);
+    printf("Enter the columns of second matrix");
+    scanf("%d",&q);
+    for(i=0;i<p;i++)
+    {
+        for(j=0;j<q;j++)
+        {
+            printf("Enter the elements of the second matrix:");
+            scanf("%d",&b[i][j]);
+        }
+    }
+    printf("\nThe second matrix is:\n");
+     for(i=0;i<p;i++)
+    {
+        for(j=0;j<q;j++)
+        {
+         printf("%d\t",b[i][j]);
+        }
+        printf("\n");
+    }
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            c[i][j]=a[i][j]-b[i][j];
+        }
+    }
+    printf("\nThe difference matrix is:\n");
+    for(i=0;i<3;i++)
+    {
+       for(j=0;j<3;j++)
+       {
+           printf("%d\t",c[i][j]);
+       }
+       printf("\n");
+    }
+    return 0;
+
+}
