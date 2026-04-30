57fc62d8edce8ed891cdbe3145293d40149129b0
Mohan1459 <157452661+Mohan1459@users.noreply.github.com>
2026-04-30 12:40:08 +0530

    Create string_name.c

---
+++ STRINGS/STRING_OF_NAMES/string_name.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+int main()
+{
+    char fname[30],lname[30];
+    printf("Enter name:");
+    scanf("%s%s",fname,lname);
+    printf("%s%s",fname,lname);
+    return 0;
+}
+
